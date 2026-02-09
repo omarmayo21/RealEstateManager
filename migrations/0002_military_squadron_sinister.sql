8dfbefc775364a178063d17cea49df467e36f767
omarmayo21 <ssole0909@gmail.com>
2026-02-09 15:32:42 +0200

    add new fields to property

---
+++ migrations/0002_military_squadron_sinister.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "units" ADD COLUMN "total_paid" integer;--> statement-breakpoint
+ALTER TABLE "units" ADD COLUMN "installment_value" integer;--> statement-breakpoint
+ALTER TABLE "units" ADD COLUMN "maintenance_deposit" integer;
