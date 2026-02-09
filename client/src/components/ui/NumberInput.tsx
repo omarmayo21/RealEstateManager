f5af388aa6f965a722c49de87fe636b7c47613dd
omarmayo21 <ssole0909@gmail.com>
2026-02-09 19:47:23 +0200

    add new fields to property

--- client/src/components/ui/NumberInput.tsx
+++ client/src/components/ui/NumberInput.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 
-type NumberInputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export interface NumberInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export function NumberInput(props: NumberInputProps) {
+export const NumberInput = React.forwardRef<
+  HTMLInputElement,
+  NumberInputProps
+>(({ className, onWheel, ...props }, ref) => {
   return (
-    <input
+    <Input
+      ref={ref}
       type="number"
-      onWheel={(e) => e.currentTarget.blur()}
+      className={cn(className)}
+      onWheel={(e) => {
+        e.currentTarget.blur();
+        onWheel?.(e);
+      }}
       {...props}
     />
   );
-}
+});
+
+NumberInput.displayName = "NumberInput";
