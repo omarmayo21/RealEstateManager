cbac05a24865c4169be4bc5c8c74bb62624b515b
omarmayo21 <ssole0909@gmail.com>
2026-02-09 16:56:32 +0200

    add new fields to property

--- server/seed.ts
+++ server/seed.ts
@@ -78,9 +78,10 @@ export default async function runApp(
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+    console.error("❌ API ERROR:", err); // اطبع الخطأ بس
 
     res.status(status).json({ message });
-    throw err;
+    
   });
 
   // importantly run the final setup after setting up all the other routes so
